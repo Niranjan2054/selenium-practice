78d580594fdce684118be4354328346b6c6f1c4f
Niranjan2054 <ckim6189@gmail.com>
2019-11-18 21:13:32 +0545

    Facebook login selenium created

--- selenium.py
+++ selenium.py
@@ -1,6 +1,11 @@
+import selenium
 from selenium import webdriver
-driver =webdriver.chrome()
-
-driver.get('https://www.google.com/')
-print(driver.title)
-driver.quit()
+driver = webdriver.Chrome
+d = driver()
+d.get('https://www.facebook.com/')
+username = d.find_element_by_name("email")
+username.send_keys('ckim6189@gmail.com')
+Pass = d.find_element_by_name('pass')
+Pass.send_keys('********') #put your password here
+submit = d.find_element_by_id('u_0_2')
+submit.click()
